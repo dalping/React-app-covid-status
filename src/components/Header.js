0290114417726156d0429d63c70416ea77b9c6c4
Dalping <kbing14@naver.com>
2021-09-03 23:58:18 +0900

    배포

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+        <h2>COVID-19</h2>
+        <select>
+          <option>국내</option>
+          <option>세계</option>
+        </select>
+      </header>
+    )
+}
